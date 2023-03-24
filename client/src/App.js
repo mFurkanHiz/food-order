b3418ab7b894150573c85f8d690f4b382a3c5ad2
mFurkanHiz <mf.wissen@gmail.com>
2023-03-24 18:01:00 +0300

    Advancing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import AdminPage from "./pages/AdminPage";
 import CartPage from "./pages/CartPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -17,6 +18,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/myorders" element={<OrdersPage />} />
+        <Route path="/admin/*" element={<AdminPage />} />
       </Routes>
     </div>
   );
